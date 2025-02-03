c010c9863ae11d159bbf1c93017d8dbc428115b2
randombackup1293 <themagicpiston145@gmail.com>
2025-02-03 15:25:24 -0500

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -68,9 +68,9 @@ local function checkExecutor()
     if identifyexecutor ~= nil and type(identifyexecutor) == "function" then
         local suc, res = pcall(function()
             return identifyexecutor()
-        end)   
-        --local blacklist = {'appleware', 'cryptic', 'delta', 'wave', 'codex', 'swift', 'solara', 'vega'}
-        local blacklist = {'solara', 'cryptic'}
+        end)
+		
+        local blacklist = {'solara', 'cryptic', 'xeno'}
         if suc then
             for i,v in pairs(blacklist) do
                 if string.find(string.lower(tostring(res)), v) then CheatEngineMode = true end
